57047866310d111a3b5c9fa7dc5984ff9f8665a1
Ben Winzer <benwinzer@Mac.fritz.box>
2026-08-07 20:03:43 +0200

    add voice samples constant for AI suggestions

---
+++ lib/constants/voice-samples.ts
@@ -0,0 +1,86 @@
+// Paste a few examples of your own writing below (emails, messages, notes — whatever
+// captures how you naturally write). The more representative samples, the better the
+// generated replies will match your voice.
+export const VOICE_SAMPLES = `yes
+
+the way i view it tho is that the stuff the brain enjoys is changeable
+
+an example: for me, i didnt like coding at all when i started my 9-5 in software engineering. then tho becuase i did it every day and learned a lot and saw the possibilities also with ai now (when you truly understand the code it writes) i switched my website building process from framer to custom code since its just way faster
+
+so you can train your mind to enjoy whatever it is you need to do
+
+but of course if i did for example blog writing or something i would be miserable doing it becuase i dont see any fun in writing blog articles at all
+
+so the meta thing you do needs to be aligned with your interests at least a little bit
+
+how do you think about it?
+
+niiice
+
+i thought it would be a chrome extension or something haha
+
+so it’s an ios app that you then kinda connect with your X account?
+
+do you use the X api to leave comments programmatically and stuff or is it more like a chrome extension?
+
+appreciate that
+
+btw sorry for the late reply
+
+how are you doing on this fabulous wednesday?
+
+ahh alright
+
+what did you study?
+
+something like computer science or osmething?
+
+ahh yeah the algo there works differently than on other social medias
+
+wdym by studying? like university or studying youtube lead gen and stuff?
+
+try reloading. or if that doesnt work close and restart your browser or device. and if that still doesnt work then i would ask TRW support :)
+
+since your service is video editing, yes. since your posts should be examples of what clients would get.
+
+simple analogy for this: if a web design agency would have a very badly designed website, you wouldnt trust them to do a good job if you become their client, right?
+
+hey G, heres my analysis of it, i hope it helps a little :)
+
+if you do a compliment it really needs to be something specific, like referencing an actual video or a line they said because otherwise it can come across as a generic or fake compliment and thats usually when people put up tgeir sales guard immediately, which makes them not reply to you in the first place since people dont wanna be sold to
+
+also in my opinion custom rap intro songs is a quite small niche, i personally dont know many people who would need a custom rap song, so youre probably reaching a lot of people who just arent really a good fit for your service, so make sure that your targeting is on point. if you are already targeting only rap artists or gamers or whatever then all good :)
+
+the other thing is you go straight from the compliment into asking if theyre interested in a custom rap song, and it happens kinda fast without much buildup at all. would be kinda like me going up to your house and then saying "your dog died". this is exaggerated of course but i hope you get the point. its: compliment (prospect thinks: okay this person looked at my content, how nice of him) -> asking them if they like rap songs (this person only wants to sell me custom rap songs so i wont answer him). thats kinda what goes on in their mind when reading the message
+
+and the offer itself is a bit vague too. "custom intro rap song" doesnt really say anything concrete. what music style of rap is it (since rap has quite a few different styles)? will it be about their content or what will it be about? what would be the benefit of having a custom rap song? more sales? how much would it cost? why should i trust you to make me a rap song in the first place?
+
+these are questions that would go on in the mind of the person receiving the dm. you can definitely make some of those questions immeidately go away if you just send a sample of a rap song you made for another client instead of first asking whether they want one since that creates friction that they have to reply to you to get more information which is not what you want if they are not sure whether to trust you or not. so including a sample or past rap song you made would be very beneficial here in my opinion
+
+since i now only gave critique here is an example of a dm you could try. of course change it to match your needs :)
+
+you mean beacuase you get reminded of being ccai captain?
+
+why cant you change your name tho?
+
+haha isnt that a cool feeling tho?
+
+do you also get in the councikl when youre captain or is that extra?
+
+yeah but cant you answer more questions and do more volume when there are more people in the chats asking questions?
+
+should we do kinda like a competition for this? to who can help out in the chats the most and become guardian first?
+
+maybe if you help in the chats basically always
+
+i maybe answer a message in the chats once a week haha
+
+so i wont become captain anytime soon haha
+
+nothing you can change about it now tho (thats how i think about sad stuff that happens, to know what i wont do in the future but not obsess over the sad thing that happened)
+
+FIRE
+
+what kind of videos?
+
+btw idk why im so high energy today haha`;
